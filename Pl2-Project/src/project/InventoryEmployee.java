2f20aa95796e1781e51b83bfd39f56e692f3e0ac
ahmednagi22 <70567575+ahmednagi22@users.noreply.github.com>
2020-12-21 22:53:00 +0200

    add_First_4_Function_In_Inventory

--- Pl2-Project/src/project/InventoryEmployee.java
+++ Pl2-Project/src/project/InventoryEmployee.java
@@ -5,10 +5,12 @@
  */
 package project;
 
+import database.ProductDB;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InventoryEmployee extends Employee {
-
+     Scanner input=new Scanner(System.in);
     public InventoryEmployee(int id, String fName, String lName, String userName, String password, String eType) {
         super(id, fName, lName, userName, password, eType);
     }
@@ -35,13 +37,13 @@ public class InventoryEmployee extends Employee {
             }
 
             switch (c) {
-                case '1':
+                case '1':newProduct();
                     break;
-                case '2':
+                case '2':deleteProduct();
                     break;
-                case '3':
+                case '3':updateProduct();
                     break;
-                case '4':
+                case '4':listProduct();
                     break;
                 case '5':
                     break;
@@ -56,5 +58,69 @@ public class InventoryEmployee extends Employee {
         System.out.println("bey bey ," + this.getfName() + "!");
         return 0;
     }
-
-}
+    public  void newProduct(){
+        System.out.printf("Enter Product Name:");
+        String name=input.nextLine();
+        System.out.printf("Enter Original Price:");
+        int OP=input.nextInt();
+        System.out.printf("Enter Discount:");
+        int diss=input.nextInt();
+        System.out.printf("Enter Amount:");
+        int amount=input.nextInt();
+        input.nextLine();
+        System.out.printf("Enter Expierd Date:");
+        String EPD=input.nextLine();
+        System.out.printf("Enter Product State:");
+        String State=input.nextLine();
+        Product d=new Product(name,OP,diss,amount,EPD,State);
+       ProductDB.add_product(d);
+    }
+    public void deleteProduct(){
+        int sn;
+        System.out.printf("Enter Product Serial Number:");
+        sn=input.nextInt();
+        ProductDB.delete_product(sn);
+    }
+    public void updateProduct(){
+        System.out.printf("Enter 1 if you want to update discount"
+                + " only and 2 for update all product info: ");
+        int choice=input.nextInt();
+        if(choice==1){
+        System.out.printf("Enter Product Serial Number:");
+        int sn=input.nextInt();
+            System.out.printf("Enter  Discount:");
+        int diss=input.nextInt();
+        ProductDB.update_product(sn, diss);
+        }
+        else if(choice==2){
+        int sn;
+        System.out.printf("Enter Product Serial Number:");
+        sn=input.nextInt();
+        input.nextLine();
+        System.out.printf("Enter new Name:");
+        String name=input.nextLine();
+        System.out.printf("Enter  Original Price:");
+        int OP=input.nextInt();
+        System.out.printf("Enter  Discount:");
+        int diss=input.nextInt();
+        System.out.printf("Enter Amount:");
+        int amount=input.nextInt();
+         input.nextLine();
+        System.out.printf("Enter Expiera Date:");
+        String EPD=input.nextLine();
+        System.out.printf("Enter Product Sate:");
+        String State=input.nextLine();
+        ProductDB.update_product(sn, name, OP, diss, amount, EPD, State);
+    }
+        else System.out.printf("Invaild Input");
+    }
+    public void listProduct(){
+       ArrayList<Product> list = new ArrayList<>();
+     list= ProductDB.get_products();
+     System.out.printf("%5s  %5s %5s  %6s %12s  %8s\n","Name","price","disscount","amount","expier data" ,"state");
+     for(int i=0;i<list.size();i++){
+        System.out.printf("%5s  %4d",list.get(i).getName(),list.get(i).getOrignalPrice());
+        System.out.printf("%7d  %8d",list.get(i).getDiscount(),list.get(i).getAmount());
+        System.out.printf("%14s  %7s\n",list.get(i).getEPD(),list.get(i).getpState());
+    }
+    }}
